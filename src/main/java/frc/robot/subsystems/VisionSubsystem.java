a46ad7daf53c517eac5e172a0bdce70678575cb0
5933 Software <jmrobotics@jmchs.org>
2025-01-14 16:44:11 -0700

    Apparently the beta photonlib works in non-beta 2025

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SubsystemBase {
+
+    // TODO: Find out the camera's actual nickname
+    PhotonCamera cam = new PhotonCamera("");
+    
+}

b6606e88cfcc76033cd38939af3e13021e46df9f
Addie Hinckley <ah20267@jmchs.org>
2025-01-20 07:49:14 -0700

    more leds

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDs {
@@ -7,18 +9,27 @@ public class LEDs {
 
 
     // Reuse buffer
-
     // Default to a length of 60, start empty output
-
     // Length is expensive to set, so only set it once, then just update data
-
     m_ledBuffer = new AddressableLEDBuffer(60);
-
     m_led.setLength(m_ledBuffer.getLength());
 
+    // Apply the LED pattern to the data buffer
+    gradient.applyTo(m_ledBuffer);
 
-    // Set the data
+    // Write the data to the LED strip
 
     m_led.setData(m_ledBuffer);
+    if () {// TODO alliance is red idk how to write it
+        m_led.setData(m_ledBuffer);
+        m_led.start();
 
-    m_led.start();}
+        LEDPattern gradient = LEDPattern.gradient(LEDPattern.GradientType.kContinuous,
+        Color.kRed, Color.kPink, Color.kCrimson, Color.kSienna, Color.kTomato, Color.kDarkOrange, 
+        Color.kDarkMagenta, Color.kFirebrick);
+        
+    // TODO blue alliance color scheme
+
+    }
+
+}

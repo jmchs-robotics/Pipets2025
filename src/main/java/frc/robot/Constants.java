41a08b4b40e1221b852c8735f57570056dc85f0a
5933 Software <jmrobotics@jmchs.org>
2025-01-31 14:13:51 -0700

    algay subsystem constuctor now exists

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -133,4 +133,15 @@ public final class Constants {
     public static final double kD = 0;
 
   }
+
+  public static final class AlgaeConstants {
+
+    public static final int flipMotorID = 3;
+    public static final int rightMotorID = 4;
+    public static final int leftMotorID = 5;
+
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+  }
 }

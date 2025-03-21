32ab211c0358d6ca823c8e58b1ebc80d877b09bc
5933software <jmrobotics@jmchs.org>
2025-03-21 17:15:29 -0600

    Had to limit the coral wheels because a neo 550 got fried when testing :(

--- src/main/java/frc/robot/subsystems/coral/CoralWheelsSubsystem.java
+++ src/main/java/frc/robot/subsystems/coral/CoralWheelsSubsystem.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.coral;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,6 +17,11 @@ public class CoralWheelsSubsystem extends SubsystemBase {
     public CoralWheelsSubsystem() {
 
         wheelMotor = new SparkMax(CoralConstants.wheelMotorID, MotorType.kBrushless);
+
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.idleMode(IdleMode.kBrake).smartCurrentLimit(10);
+
+        wheelMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         
     }
 

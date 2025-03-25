ee9c5efe55ff826bcfafa6e5ddd274d1dc08e83b
5933software <jmrobotics@jmchs.org>
2025-03-25 09:30:36 -0600

    forgot to actually apply the config to the climber motor

--- src/main/java/frc/robot/subsystems/climbers/ClimbersSubsystem.java
+++ src/main/java/frc/robot/subsystems/climbers/ClimbersSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.climbers;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
@@ -16,6 +17,9 @@ public class ClimbersSubsystem extends SubsystemBase {
 
         TalonFXConfiguration config = new TalonFXConfiguration();
         config.OpenLoopRamps.DutyCycleOpenLoopRampPeriod = 1;
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+        climbMotor.getConfigurator().apply(config);
 
     }
 
